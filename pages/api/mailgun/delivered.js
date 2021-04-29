2f1d354593d0adc3edaeee1c3cc739e5c0db632c
Nick Hyde <nick.hyde@funeralguide.co.uk>
2021-04-29 08:47:48 +0100

    Add mongo db functionality

--- pages/api/mailgun/delivered.js
+++ pages/api/mailgun/delivered.js
@@ -1,8 +1,32 @@
 const crypto = require('crypto');
+const mongoose = require('mongoose');
 
+const mongoDB = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 const DOMAIN = 'mg.funeralguide.co.uk';
 
+const eventSchema = new mongoose.Schema({
+  messageId: String,
+  to: String,
+  from: String,
+  subject: String,
+});
+const Event = mongoose.model('Event', eventSchema);
+
 export default (req, res) => {
+
+  mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log('DB connected');
+  });
+
+  // Event.find(function (err, events) {
+  //   if (err) return console.error(err);
+  //   console.log(events);
+  // })
+
   const { 'event-data': eventData } = req.body;
   const { timestamp, token, signature } = req.body.signature || {};
 
@@ -21,6 +45,17 @@ export default (req, res) => {
     token,
     signature,
   });
+
+  if (result) {
+    const { message } = eventData || {};
+    const { headers } = message;
+    const { 'message-id': messageId, to, from, subject } = headers || {};
+
+    const event = new Event({ messageId, to, from, subject });
+    event.save(function (err, event) {
+      if (err) return console.error(err);
+    });
+  }
 
   res.status(200).json({ success: result });
 }
